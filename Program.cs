b0e6b69ba9078e9f58e123e1100663fc7ef32368
lucasLmendes <lucaslmendesdev@gmail.com>
2024-10-31 23:17:06 -0300

    Adding the initial logic for the application. Created a CRUD model to be followed.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to LibCage System!\n");
+        Console.WriteLine("Please select an option: ");
+        Console.WriteLine("\n1. Add a book to the library.");
+        Console.writeLine("\n2. Find a book.");
+        Console.WriteLine("\n3. Change information about a book.");
+        Console.WriteLine("\n4. Delete a book from the library.");
+        Console.WriteLine("\n5. Close the application.");
+    }
+
+    public static void menuSelect()
+    {
+        string? choiceInput = Console.ReadLine();
+
+        while (choiceInput != null)
+        {
+            switch (choiceInput.Value)
+            {
+                case "1"
+                {
+                    Console.WriteLine("Adding a new book!");
+                    
+                }
+            }
+        }
+    }
+}
