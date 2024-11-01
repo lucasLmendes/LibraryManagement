64152bf973e13f53c7fcba34fa56a95716d77eb2
lucasLmendes <lucaslmendesdev@gmail.com>
2024-10-31 23:27:45 -0300

    Completed the skeleton for the CRUD part of the application.

--- Program.cs
+++ Program.cs
@@ -22,10 +22,39 @@ public static class Program
         {
             switch (choiceInput.Value)
             {
-                case "1"
+                case "1":
                 {
                     Console.WriteLine("Adding a new book!");
-                    
+                    // logic for adding the book in the DB
+                    break;
+                }
+                case "2":
+                {
+                    Console.WriteLine("Please, write the name of the book you want to find: ");
+                    // logic for the search function inside the database
+                    break;
+                }
+                case "3":
+                {
+                    Console.WriteLine("Please insert the name of the book you want to change or mark as borrowed: ");
+                    // logic for changing information of a book in the table of books. TODO: add a row in the database to mark if a book is borrowed or not (boolean?)
+                    break;
+                }
+                case "4":
+                {
+                    Console.WriteLine("Remove a book from the application.");
+                    // logic for book removal from the database
+                    break;
+                }
+                case "5":
+                {
+                    Console.WriteLine("Exiting the application."); 
+                    //logic for exiting the app (probably change the parameter for while loop)
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Please insert a number from 1 to 5!");
                 }
             }
         }
